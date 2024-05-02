2f8487aafb01e7b699008977f370a6ceb34c427d
mdmohan89 <121094372+mdmohan89@users.noreply.github.com>
2024-05-02 17:37:12 +0530

    Update postgressql.tf
    update the instruction

--- postgressql.tf
+++ postgressql.tf
@@ -1,3 +1,24 @@
+# Prerequisites
+# Before you proceed ensure you have the following prerequisites:
+
+# Terraform installed on your local machine.
+# An Azure account with appropriate permissions to create resources.
+# Basic knowledge of Terraform and Azure concepts.
+# Terraform Configuration
+# The configuration includes the following resources:
+
+# Resource Group (azurerm_resource_group): Defines the resource group where all Azure resources will be deployed.
+# Virtual Network (azurerm_virtual_network): Defines the virtual network to which the PostgreSQL server will be connected.
+# Subnet (azurerm_subnet): Defines the subnet within the virtual network for the PostgreSQL server.
+# PostgreSQL Server (azurerm_postgresql_server): Defines the PostgreSQL server instance, including server name, SKU, storage, and credentials.
+# Virtual Network Rule (azurerm_postgresql_virtual_network_rule): Defines a virtual network rule to allow access to the PostgreSQL server from specified subnets.
+# Usage
+
+# Follow these steps to deploy the PostgreSQL database:
+
+# Clone Repository: Clone this repository to your local machine:
+
+
 resource "azurerm_resource_group" "example" {
   name     = var.resource-group
   location = var.region
